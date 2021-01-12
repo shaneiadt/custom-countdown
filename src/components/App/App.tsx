c4ff21b6092c6d46bf3cbab96181967d8d1fe0c1
Shane O'Moore <shaneiadt@gmail.com>
2021-01-12 19:13:32 +0000

    conditional render Background component and update css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <Background />
+      <Background video="/time.mp4" />
     </div>
   );
 }
--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,13 +1,17 @@
 import { h, Fragment } from 'preact';
 import './Background.css';
 
-export default () => {
-    return(
-      <Fragment>
-        <video class="video-background" loop muted autoPlay>
-          <source src="/time.mp4"></source>
-        </video>
-        <div className="video-overlay"></div>
-      </Fragment>
-    );
+export default ({ video = '' }: { video?: string }) => {
+  return (
+    <Fragment>
+      {video.length > 1 &&
+        <div>
+          <video class="video-background" loop muted autoPlay>
+            <source src={video}></source>
+          </video>
+          <div className="video-overlay"></div>
+        </div>
+      }
+    </Fragment>
+  );
 };
