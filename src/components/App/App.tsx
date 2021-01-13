66fd66e8b98d6f3a87ad10a9e68c8171b331a765
Shane O'Moore <shaneiadt@gmail.com>
2021-01-13 15:16:27 +0000

    Container component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import { h } from 'preact';
 import './App.css';
 
 import Background from '../Background/Background';
+import Container from '../Container/Container';
 
 function App() {
 
   return (
     <div className="App">
       <Background video="/time.mp4" />
+      <Container />
     </div>
   );
 }
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,8 @@
+import { h } from 'preact';
+import './Container.css';
+
+export default () => {
+  return (
+    <div class="container"></div>
+  );
+};
