09a0d1cf6d129de8717a4c202fd63b2d390b0a87
Shane O'Moore <shaneiadt@gmail.com>
2021-01-14 11:32:16 +0000

    Countdown component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,14 +1,16 @@
 import { h } from 'preact';
 import Form from '../Form/Form';
+import Countdown from '../Countdown/Countdown';
 import './Container.css';
 
 export default () => {
   return (
     <div className="container">
-      <div className="input-container" id="input-container">
+      <div className="input-container" id="input-container" hidden>
         <h1>Create a Custom Countdown</h1>
         <Form />
       </div>
+      <Countdown />
     </div>
   );
 };
