e89325dc6a2a70cf50c8dab2e26cbf76251dd6f8
Shane O'Moore <shaneiadt@gmail.com>
2021-01-16 12:32:35 +0000

    start setting up component state

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,15 +1,22 @@
 import { h, Fragment } from 'preact';
+import { useState } from 'preact/hooks';
 import Form from '../Form/Form';
 import Countdown from '../Countdown/Countdown';
 import Complete from '../Complete/Complete';
 import './Container.css';
 
 export default () => {
+  const [view, setView] = useState(<Form submit={submit} />);
+
+  function submit(e: Event) {
+    e.preventDefault();
+    console.log('SUBMITTED IT!');
+    setView(<Countdown />);
+  }
+
   return (
     <Fragment>
-      <Form />
-      {/* <Countdown show={true} />
-      <Complete show={false} /> */}
+      {view}
     </Fragment>
   );
 };
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,21 +1,29 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import './Form.css';
 
-export default () => {
-    function submit(e: Event) {
-        e.preventDefault();
-        console.log('FORM SUBMITTED');
+export default ({ submit }: { submit: (e: Event) => void }) => {
+    const [title, setTitle] = useState('');
+
+    const updateTitle = (e: Event) => {
+        const { value } = e.target as HTMLInputElement;
+        setTitle(value);
     }
+    
+    const updateDate = (e: Event) => {
+        console.log(e);
+    }
+
     return (
         <div className="container">
             <div className="input-container" id="input-container">
                 <h1>Create a Custom Countdown</h1>
                 <form onSubmit={submit} className="form" id="countdownForm">
                     <label htmlFor="title">Title</label>
-                    <input type="text" id="title" placeholder="What are you counting down to?" />
+                    <input type="text" id="title" placeholder="What are you counting down to?" onInput={updateTitle} value={title} />
 
                     <label htmlFor="date-picker" className="date-picker">Select a Date</label>
-                    <input type="date" id="date-picker" />
+                    <input type="date" id="date-picker" onChange={updateDate} />
 
                     <button type="submit">Submit</button>
                 </form>
