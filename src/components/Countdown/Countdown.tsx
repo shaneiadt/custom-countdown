2f664bfde83d8fc2fcba5508e899c9b5aaa6c9d6
Shane O'Moore <shaneiadt@gmail.com>
2021-01-18 08:26:38 +0000

    sprinkle some state into Countdown component

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -1,33 +1,53 @@
 import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 import './Countdown.css';
 
 export default ({ title, date, done, reset }: { title: string, date: string, done: () => void, reset: () => void }) => {
 
   const countDownValue = new Date(date).getTime();
-  const distance = countDownValue - new Date().getTime();
+  const [display, setDisplay] = useState(false);
+  const [time, setTime] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('INTERVAL');
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
 
-  const days = Math.floor(distance / day);
-  const hours = Math.floor((distance % day) / hour);
-  const minutes = Math.floor((distance % hour) / minute);
-  const seconds = Math.floor((distance % minute) / second);
+      let distance = countDownValue - new Date().getTime();
 
-  return (
+      const days = Math.floor(distance / day);
+      const hours = Math.floor((distance % day) / hour);
+      const minutes = Math.floor((distance % hour) / minute);
+      const seconds = Math.floor((distance % minute) / second);
+
+      setTime({
+        days,
+        hours,
+        minutes,
+        seconds
+      });
+
+      if (!display) setDisplay(true);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (display ?
     <div className="container">
       <div className="countdown" id="countdown">
         <h1 id="countdown-title">{title}</h1>
         <ul>
-          <li><span>{days}</span>Days</li>
-          <li><span>{hours}</span>Hours</li>
-          <li><span>{minutes}</span>Minutes</li>
-          <li><span>{seconds}</span>Seconds</li>
+          <li><span>{time.days}</span>Days</li>
+          <li><span>{time.hours}</span>Hours</li>
+          <li><span>{time.minutes}</span>Minutes</li>
+          <li><span>{time.seconds}</span>Seconds</li>
         </ul>
         <button id="countdown-button" onClick={reset}>Reset</button>
       </div>
-    </div>
+    </div> : null
   );
 };
